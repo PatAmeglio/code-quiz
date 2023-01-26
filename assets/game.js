fa2ce596d9b673087e89494173c1f360fbef046c
Patrick Ameglio <pameglio101@yahoo.com>
2023-01-26 12:27:08 -0500

    fixing Javascript code to show correct or incorrect

--- assets/game.js
+++ assets/game.js
@@ -1,9 +1,10 @@
 const startButton = document.querySelector("#start-button");
 const questionsContainer = document.querySelector(".questions");
-const answersContainer = document.querySelector(".answers");
+const answersContainer = document.querySelectorAll(".answers button");
 const scoreContainer = document.querySelector("#score");
 const initialsInput = document.querySelector("#initials-input");
 const saveScoreButton = document.querySelector("#save-score-button");
+const result = document.querySelector("#result");
 
 let timer = 60;
 let score = 0;
@@ -11,24 +12,24 @@ let currentQuestionIndex = 0;
 
 const questions = [
   {
-    question: "What is the correct syntax for creating an object in JavaScript?",
+    question: "What does the abbreviation HTML stand for?",
     answers: [
-      "object myObject = {}",
-      "var myObject = new Object()",
-      "var myObject = {}",
-      "myObject = {}"
+      "All of the below",
+      "HighText Markup Language",
+      "HyperText Markdown Language",
+      "HyperText Markup Language"
     ],
-    correctAnswer: "var myObject = {}"
+    correctAnswer: "HyperText Markup Language"
   },
   {
-    question: "What is the correct syntax for an array in JavaScript?",
+    question: "What is the correct syntax to declare a variable in JavaScript?",
     answers: [
-      "var myArray = (1, 2, 3)",
-      "var myArray = [1, 2, 3]",
-      "var myArray = 1, 2, 3",
-      "var myArray = new Array(1, 2, 3)"
+      "var myVariable = 1, 2, 3",
+      "myVariable var = 1, 2, 3",
+      "myVariable = 1, 2, 3",
+      "var = myVariable 1, 2, 3"
     ],
-    correctAnswer: "var myArray = [1, 2, 3]"
+    correctAnswer: "var myVariable = 1, 2, 3"
   },
   {
     question: "What is the correct syntax for a function in JavaScript?",
@@ -39,17 +40,27 @@ const questions = [
       "var myFunction()"
     ],
     correctAnswer: "function myFunction()"
+  },
+  {
+    question: "What does CSS stand for?",
+    answers: [
+      "Cross Site Scripting",
+      "Communications Support System",
+      "Cascading Style Sheets",
+      "Customer Satisfaction Survey"
+    ],
+    correctAnswer: "Cascading Style Sheets"
   }
 ];
 
 startButton.addEventListener("click", startGame);
-answersContainer.addEventListener("click", checkAnswer);
+answersContainer.forEach(button => button.addEventListener("click", checkAnswer));
 saveScoreButton.addEventListener("click", saveScore);
 
 function startGame() {
   startButton.style.display = "none";
   questionsContainer.style.display = "block";
-  answersContainer.style.display = "block";
+  answersContainer.forEach(button => button.style.display = "block");
   scoreContainer.style.display = "block";
   setTimer();
   displayQuestion();
@@ -59,10 +70,10 @@ function setTimer() {
   const timerInterval = setInterval(() => {
     timer--;
     scoreContainer.textContent = `Timer: ${timer}`;
-    if (timer === 0) {
+    if (timer <= 0) {
       clearInterval(timerInterval);
       endGame();
-    }
+    }    
   }, 1000);
 }
 
@@ -70,32 +81,33 @@ function displayQuestion() {
   const question = questions[currentQuestionIndex];
   questionsContainer.textContent = question.question;
   for (let i = 0; i < 4; i++) {
-    const answerButton = document.querySelector(`#answer${i+1}`);
-    answerButton.textContent = question.answers[i];
+    answersContainer[i].textContent = question.answers[i];
   }
 }
 
 function checkAnswer(event) {
   const selectedButton = event.target;
   const correctAnswer = questions[currentQuestionIndex].correctAnswer;
-  if (selectedButton.textContent === correctAnswer) {
+  if (selectedButton.dataset.answer === correctAnswer) {
     score++;
-    alert("Correct!");
-  } else {
+    result.textContent ="Correct!";
+  } 
+  else {
     timer -= 10;
-    alert("Incorrect!");
+    result.textContent ="Incorrect!";
   }
   currentQuestionIndex++;
   if (currentQuestionIndex === questions.length) {
     endGame();
   } else {
     displayQuestion();
+    result.textContent();
   }
 }
 
 function endGame() {
   questionsContainer.style.display = "none";
-  answersContainer.style.display = "none";
+  answersContainer.forEach(button => button.style.display = "none");
   scoreContainer.style.display = "none";
   initialsInput.style.display = "block";
   saveScoreButton.style.display = "block";
@@ -109,3 +121,4 @@ function saveScore() {
   localStorage.setItem("highscores", JSON.stringify(highscores));
   window.location.href = "./highscore.html";
 }
+
